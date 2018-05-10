87e383c6d4efa44bd0734e0ca1bb07a517269b11
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-09 21:32:37 -0300

    change eslint

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,29 +1,25 @@
-
+import React from 'react';
 import Home from './home';
 import Footer from './footer/Footer';
 import Projects from './projects/Projects';
-import About from "./about/About";
-import Menu from "./menu/Menu";
+import About from './about/About';
+import Menu from './menu/Menu';
 import Skills from './skills/Skills';
 import Video from './../components/Video';
-import React from 'react';
 
-const App = () => {
-	return (
+const App = () => (
 
-		<div className='main-container'>
-			<Menu />
-			<Home />
-			<About />
-			<Skills />
-			<Projects />
-			<Footer />
-			<Video />
-		</div>
+  <div className="main-container">
+    <Menu />
+    <Home />
+    <About />
+    <Skills />
+    <Projects />
+    <Footer />
+    <Video />
+  </div>
 
-	);
-};
-
+);
 
 
 export default App;
