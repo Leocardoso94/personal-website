63b6a4ec4b87ca544aaa84d5342694db60469525
Leonardo Cardoso <leocardosoti@gmail.com>
2018-07-16 20:17:45 -0300

    buscando artigos direto no medium

--- src/containers/articles/index.js
+++ src/containers/articles/index.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import './articles.css';
-import artigosData from './artigos-data';
 
-const ArtigosList = () =>
-  artigosData.map(({ title, url, image }) => (
-    <li key={url} className="articles__list__item">
-      <a href={url} target="_blank">
-        <img src={image} alt={title} />
+const getFeed = async () => {
+  const url = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@leocardoso';
+
+  const response = await fetch(url, {});
+  const json = await response.json();
+  return json;
+};
+
+const ArtigosList = ({ articles }) =>
+  articles.map(({ title, thumbnail, link }) => (
+    <li key={link} className="articles__list__item">
+      <a href={link} target="_blank">
+        <img src={thumbnail} alt={title} />
         <h3 className="articles__list__item--title">{title}</h3>
       </a>
     </li>
   ));
 
-export default () => (
-  <div id="Artigos">
-    <div className="container">
-      <h1 className="text-center cursive">Artigos</h1>
-      <h4 className="text-center">Alguns artigos já publicados:</h4>
-      <br />
-      <ul className="articles__list">
-        <ArtigosList />
-      </ul>
-    </div>
-  </div>
-);
+export default class extends React.Component {
+  state = {
+    articles: []
+  };
+  componentDidMount = async () => {
+    const feed = await getFeed();
+    this.setState({ articles: feed.items.filter(item => item.categories.length > 0) });
+  };
+  render() {
+    return (
+      <div id="Artigos">
+        <div className="container">
+          <h1 className="text-center cursive">Artigos</h1>
+          <h4 className="text-center">Alguns artigos já publicados:</h4>
+          <br />
+          <ul className="articles__list">
+            <ArtigosList articles={this.state.articles} />
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
